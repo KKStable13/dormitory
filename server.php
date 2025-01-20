217b6f694897cf875e3321584fa8cace9506f1cb
KKStable13 <143015378+KKStable13@users.noreply.github.com>
2025-01-20 15:34:46 +0700

    test

--- server.php
+++ server.php
@@ -5,6 +5,11 @@
 //$password = "";
 //$dbname = "dormitory_system";
 
+$servername = "deploywebapp2.mysql.database.azure.com";
+$username = "KKStable";
+$password = "Phetkho13112545";
+$dbname = "dormitory";
+
 // Create Connection
 $con = mysqli_connect($servername, $username, $password, $dbname);
 
