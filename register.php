7d03be0d492c109e37609556ff94e38c1345a140
KKStable13 <143015378+KKStable13@users.noreply.github.com>
2025-01-21 09:44:13 +0700

    test

--- register.php
+++ register.php
@@ -1,10 +1,21 @@
 <?php
 session_start();
-include('server.php');
+    include('server.php');
+?>
+
+<?php
 $servername = "deploywebapp2.mysql.database.azure.com";
 $username = "KKStable";
 $password = "Phetkho13112545";
 $dbname = "dormitory";
+
+// Create Connection
+$con = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
 
 <!DOCTYPE html>
