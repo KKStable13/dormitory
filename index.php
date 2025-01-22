39c988934fdac6d1a5be406862340ca1966c4907
KKStable13 <143015378+KKStable13@users.noreply.github.com>
2025-01-22 16:39:56 +0700

    test

--- index.php
+++ index.php
@@ -12,10 +12,10 @@ $servername = "deploywebapp2.mysql.database.azure.com";
 $username = "KKStable";
 $password = "Phetkho13112545";
 $dbname = "dormitory";
-$port = "3306";
+//$port = "3306";
 
 // Create Connection
-$con = mysqli_connect($servername, $username, $password, $dbname, $port);
+$con = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
 if (!$con) {
