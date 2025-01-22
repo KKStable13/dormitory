6d5a8e4bf7c2431185382c02da5ebc2c1bf0e906
KKStable13 <143015378+KKStable13@users.noreply.github.com>
2025-01-22 16:44:01 +0700

    test

--- server.php
+++ server.php
@@ -1,15 +1,15 @@
 <?php
 
-//$servername = "localhost";
-//$username = "root";
-//$password = "";
-//$dbname = "dormitory";
-//$dbname = "dormitory_system";
-
-$servername = "deploywebapp2.mysql.database.azure.com";
-$username = "KKStable";
-$password = "Phetkho13112545";
+$servername = "localhost";
+$username = "root";
+$password = "";
 $dbname = "dormitory";
+$dbname = "dormitory_system";
+
+//$servername = "deploywebapp2.mysql.database.azure.com";
+//$username = "KKStable";
+//$password = "Phetkho13112545";
+//$dbname = "dormitory";
 //$port = "3306";
 
 // Create Connection
